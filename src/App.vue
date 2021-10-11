5032a464d99c725a845511ac70ba8df9ec33701d
Ramsey Feng <ramsey.feng@boehringer-ingelheim.com>
2021-10-11 10:24:33 +0800

    Initial vite project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button>hi</button>
+  <el-button>eleplus</el-button>
+</template>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "element-plus/packages/theme-chalk/src/base.scss";
+
+createApp(App).mount('#app')
